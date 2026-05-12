6e15544cd862fde5c4db313c8fdfd46f169d65b8
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-05-12 22:56:27 +0600

    upload radio button

---
+++ radioButton.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Choose Your Course')
+
+st.radio('Choose your course',
+         ['DSA', 
+          'Algorithm',
+          'Machine Learning',
+          'Deep Learning',
+          ], index= None)
